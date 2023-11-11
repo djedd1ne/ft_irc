4928be669644d380db3e5bba4810020752af9d25
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-11-11 16:39:13 +0100

    tried something

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -16,6 +16,8 @@
 #include <vector>
 #include <poll.h>
 
+#define CONNECTIONS 10
+
 int main(int argc, char **argv)
 {
 	//do memory management before exits in Server (if exists);
@@ -25,7 +27,8 @@ int main(int argc, char **argv)
 	Input		input(argc, argv);
 	Server		server;
 	addrinfo	*test;
-	pollfd	conn;
+	pollfd		conn[CONNECTIONS];
+	int			existingConns;
 
 	(void)conn;
 	input.parseInput();
@@ -37,11 +40,32 @@ int main(int argc, char **argv)
     std::cout << "Server is listening on port " << input.getPort() << std::endl;
 
 	int clientsocket;
+	server.start_listening(socket);
 
-    while (1)
+	conn[0].fd = socket;
+	conn[0].events = POLLIN;
+	existingConns = 1;
+	while(1)
 	{
-		server.start_listening(socket);
-		clientsocket = server.accept_conn(socket, &test);
-		server.read_messages(clientsocket);
+		poll(conn, CONNECTIONS, 200);
+		//iterate over conns
+		for (int i = 0; i < existingConns; i++)
+		{
+			if (conn[i].revents & POLLIN)
+			{
+				if (conn[i].fd == socket)
+				{
+					clientsocket = server.accept_conn(socket, &test);
+					conn[i].fd = clientsocket;
+					conn[i].events = POLLIN;
+					existingConns++;
+					std::cout << "New connection accepted: "<<std::endl;
+				}
+				else
+				{
+					server.read_messages(conn[i].fd);
+				}
+			}
+		}
 	}
 }
